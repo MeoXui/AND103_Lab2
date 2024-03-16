a231911b77b4cb76c0b74be1a586bc165c1d667b
MeoXui <130195222+MeoXui@users.noreply.github.com>
2024-03-16 15:53:16 +0700

    $ git add

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'Lab 2 - AND 103',
+    title: 'Lab - AND 103',
     class: 'MD18303',
-    name: 'Huynk',
+    student: 'Huynk',
     id: 'PH38086'
   });
 });
